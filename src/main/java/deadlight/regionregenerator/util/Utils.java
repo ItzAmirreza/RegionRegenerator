b0bbddec29ebcdfeba9e912d3e6a676a73d092ac
Dead_Light <amirrezawt@gmail.com>
2020-07-07 01:14:36 +0300

    Added 2 new object `mainprefix` & `chatprefix` to utils + added sound after breaking a block

--- src/main/java/deadlight/regionregenerator/util/Utils.java
+++ src/main/java/deadlight/regionregenerator/util/Utils.java
@@ -6,4 +6,10 @@ public class Utils {
     public static String chat(String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
+
+    //Prefix used for chat / rr command etc..
+    public static String chatprefix = "&8[&eR&bR&8]&r";
+    //Prefix used for main messages (Mainly Console)
+    public static String mainprefix = "&8[&eR&bR&8]&r";
+    
 }
